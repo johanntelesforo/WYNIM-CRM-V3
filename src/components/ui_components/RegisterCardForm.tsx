1800eab4f648a76022c764639911467982b3c309
johanntelesforo <jtelesforo_2000000000102@uic.edu.ph>
2024-04-23 01:08:13 +0800

    Changes to the aesthetic of the website

--- src/components/ui_components/RegisterCardForm.tsx
+++ src/components/ui_components/RegisterCardForm.tsx
@@ -1,5 +1,4 @@
 import { TextInput } from '@tremor/react';
-import { Card } from '@tremor/react';
 import { useNavigate } from 'react-router-dom';
 
 function RegisterCardForm() {
@@ -13,43 +12,39 @@ function RegisterCardForm() {
   return (
     <div>
       <div className='flex items-center justify-center h-96 p-96 text-center mt-16'>
-        <Card
-          decoration="top"
-          decorationColor="indigo">
-          <form action="">
-            <div className="mx-auto max-w-sm space-y-6">
-              <div className='m-6 text-center items-center justify-center'>
-                <h1 className="text-2xl font-bold"> Fill out the necessary details needed </h1>
-              </div>
-              <div>
-                <h2 className='text-base m-2'> First Name </h2>
-                <TextInput placeholder='e.g Juan' type='text' className='text-base p-2' id='fname' />
-              </div>
-              <div>
-                <h2 className='text-base m-2'> Last Name </h2>
-                <TextInput placeholder='e.g Dela Cruz' type='text' className='text-base p-2' id='lname' />
-              </div>
-              <div>
-                <h2 className='text-base m-2'> Email </h2>
-                <TextInput placeholder='Email' type='email' className='text-base p-2' id='email' />
-              </div>
-              <div>
-                <h2 className='text-base m-2'> Password </h2>
-                <TextInput placeholder='Superawesomepassword' type="password" className='text-base p-2' id='password' />
-              </div>
-              <div>
-                <h2 className='text-base m-2'> Confirm Password </h2>
-                <TextInput placeholder='Superawesomepassword' type="password" className='text-base p-2' id='password' />
-              </div>
+        <form action="">
+          <div className="mx-auto max-w-sm space-y-6">
+            <div className='m-6 text-center items-center justify-center'>
+              <h1 className="text-2xl font-bold"> Fill out the necessary details needed </h1>
             </div>
-            <div className="flex items-center justify-center mt-12 mb-6 w-full">
-              <button className="btn btn-primary text-base btn-wide" onClick={handleSignup}> Signup </button>
+            <div>
+              <h2 className='text-base m-2'> First Name </h2>
+              <TextInput placeholder='e.g Juan' type='text' className='text-base p-2' id='fname' />
             </div>
-          </form>
-          <div>
-            <p className="text-center text-sm mt-4">Already have an account? <a href="/login" className="text-blue-500">Login</a></p>
+            <div>
+              <h2 className='text-base m-2'> Last Name </h2>
+              <TextInput placeholder='e.g Dela Cruz' type='text' className='text-base p-2' id='lname' />
+            </div>
+            <div>
+              <h2 className='text-base m-2'> Email </h2>
+              <TextInput placeholder='Email' type='email' className='text-base p-2' id='email' />
+            </div>
+            <div>
+              <h2 className='text-base m-2'> Password </h2>
+              <TextInput placeholder='Superawesomepassword' type="password" className='text-base p-2' id='password' />
+            </div>
+            <div>
+              <h2 className='text-base m-2'> Confirm Password </h2>
+              <TextInput placeholder='Superawesomepassword' type="password" className='text-base p-2' id='password' />
+            </div>
           </div>
-        </Card>
+          <div className="flex items-center justify-center mt-12 mb-6 w-full">
+            <button className="btn btn-primary text-base btn-wide" onClick={handleSignup}> Signup </button>
+          </div>
+        </form>
+        <div>
+          <p className="text-center text-sm mt-4">Already have an account? <a href="/login" className="text-blue-500">Login</a></p>
+        </div>
       </div>
     </div>
   );
--- src/router.tsx
+++ src/router.tsx
@@ -34,7 +34,8 @@ const router = createBrowserRouter([
             {
                 path: "programsandschools",
                 element: <ProgramsAndSchools />
-            }
+            },
+
         ]
     }
 ]);
